06eaaee238b85f7541a4c483c3fdf1fac81f0459
mqnu00 <647983952@qq.com>
2024-02-21 00:51:31 +0800

    添加http get方法。修改成api访问

---
+++ app/src/main/java/com/lzh/kanmeitu/util/HttpUtils.java
@@ -0,0 +1,29 @@
+package com.lzh.kanmeitu.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpUtils {
+
+    public static String getHttpResult(String urlStr){
+        try {
+            URL url=new URL(urlStr);
+            HttpURLConnection connect=(HttpURLConnection)url.openConnection();
+            InputStream input=connect.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            String line = null;
+            System.out.println(connect.getResponseCode());
+            StringBuffer sb = new StringBuffer();
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+}
